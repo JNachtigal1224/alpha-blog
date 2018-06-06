4046d5c85f00fc6cc77052f51a6d3f79621f8ac3
Jarret <jarretn1224@gmail.com>
2018-06-06 16:57:14 -0400

    fixed create, and flash and error messages, create show

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+    render 'new'
+    end
+  end
+
+  def show
+    #trying to show a specific article (needs id)
+    @article = Article.find(params[:id])
+  end
+
+  def index
+
+  end
+
+  private
+    def article_params
+      #whitelists
+      params.require(:article).permit(:title, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #get 'pages/home', to: 'pages#home'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  #gives CRUD paths
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
